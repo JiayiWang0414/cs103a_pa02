d3303377b299941a265d0590b6b21b80d59f4f60
JiayiWang0414 <99427888+JiayiWang0414@users.noreply.github.com>
2022-03-24 15:22:58 -0400

    fix the transaction.py

--- cs103a-pa02/pa02/transaction.py
+++ cs103a-pa02/pa02/transaction.py
@@ -7,20 +7,18 @@ import sqlite3
 class Transaction:
     ''' connects with the sqlite database and create add, select, and delete methods --Bohan'''
 
-
+    # connect to the database and create the five fields: item_num, amount, category, date, description
     def __init__(self, dbfile):
-        '''connect to the database and create the five fields: item_num, amount, category, date, description'''
         con = sqlite3.connect(dbfile)
         cur = con.cursor
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions
-                    (item_num text, amount int, category text, date int, description text)''')
+                    (item_num text, amount int, category text, date text, description text)''')
         con.commit()
         con.close()
         self.dbfile = dbfile
 
-    
+    # return all the transactions in the database 
     def select_all(self):
-        '''return all the transactions in the database '''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("""SELECT * from transactions""")
@@ -28,9 +26,8 @@ class Transaction:
         con.close()
         return [dict(row) for row in rows]
 
-    
+    # return one transaction in the database
     def select_one(self, id):
-        '''return one transaction in the database'''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("""SELECT * FROM transactions WHERE id = ?""", (id,))
@@ -38,9 +35,8 @@ class Transaction:
         con.close()
         return dict(row)
 
-    
+    # add a transaction to the database
     def add(self, item):
-        '''add a transaction to the database'''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("""INSERT INTO transactions VALUES(?,?,?,?,?)""",
@@ -48,9 +44,8 @@ class Transaction:
         con.commit()
         con.close()
 
-    
+    # delete a transaction from the database
     def delete(self, id):
-        '''delete a transaction from the database'''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("""DELETE FROM transactions WHERE id = ?""", (id,))
