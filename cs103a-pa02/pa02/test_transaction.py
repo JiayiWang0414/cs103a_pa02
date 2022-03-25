fcfe9ed47e6f8cf4a2ad634ac2cc75ed2db31905
bohan0lin <63618439+bohan0lin@users.noreply.github.com>
2022-03-24 22:21:31 -0400

    update pylint and pytest

--- cs103a-pa02/pa02/test_transaction.py
+++ cs103a-pa02/pa02/test_transaction.py
@@ -4,18 +4,18 @@ from transaction import Transaction
 
 @pytest.fixture
 def dbfile(tmpdir):
-    '''create a database file in a temporary file'''
+    '''create a database file in a temporary file --Bohan'''
     return tmpdir.join('test_tracker.db')
 
 @pytest.fixture
 def empty_db(dbfile):
-    '''create an empty database'''
+    '''create an empty database --Bohan'''
     d_b = Transaction(dbfile)
     yield d_b
 
 @pytest.fixture
 def small_db(empty_db):
-    ''' create a small database, and tear it down later'''
+    ''' create a small database, and tear it down later --Bohan'''
     cat1 = {'item_num': '1', 'amount': 10, 'category': 'food', 'date': 20220320, 'description': 'groceries and takeout '}
     cat2 = {'item_num': '2', 'amount': 2, 'category': 'car', 'date': 20220320, 'description': 'gas and repairs'}
     cat3 = {'item_num': '3', 'amount': 5, 'category': 'fun', 'date': 20220320, 'description': 'movies and dining out'}
@@ -27,11 +27,28 @@ def small_db(empty_db):
     empty_db.delete(id2)
     empty_db.delete(id1)
 
+@pytest.mark.selectone
+def test_select_one(small_db):
+    result = small_db.select_one('3')
+    assert result == {'item_num': '3', 'amount': 5, 'category': 'fun', 'date': 20220320, 'description': 'movies and dining out'}
+
+@pytest.mark.selectall
+def test_select_all(small_db):
+    result = small_db.select_all()
+    assert result == [{'item_num': '1', 'amount': 10, 'category': 'food', 'date': 20220320, 'description': 'groceries and takeout '},
+                    {'item_num': '2', 'amount': 2, 'category': 'car', 'date': 20220320, 'description': 'gas and repairs'},
+                    {'item_num': '3', 'amount': 5, 'category': 'fun', 'date': 20220320, 'description': 'movies and dining out'}]
+
 @pytest.mark.add
 def test_add(small_db):
-    '''test the add method in the transaction class'''
-    cat0 = {'item_num': '4', 'amount': 15, 'category': 'food', 'date': 20220320, 'description': 'testing'}
-    
+    '''test the add method in the transaction class --Bohan'''
+    tran0 = {'item_num': '4', 'amount': 15, 'category': 'food', 'date': 20220321, 'description': 'testing'}
+    tran1 = small_db.select_all()
+    small_db.add(tran0)
+    tran2 = small_db.select_all()
+    assert len(tran1) + 1 == len(tran2)
+    tran3 = small_db.select_one('4')
+    assert tran3 == {'item_num': '4', 'amount': 15, 'category': 'food', 'date': 20220321, 'description': 'testing'}
     
 @pytest.mark.delete
 def test_delete(small_db):
--- cs103a-pa02/pa02/transaction.py
+++ cs103a-pa02/pa02/transaction.py
@@ -6,9 +6,11 @@ import sqlite3
 
 #to_trans_dict -- Charlotte
 def to_trans_dict(trans_tuple):
-    ''' trans is a transaction tuple ( item_num, amount, category, date, desc)'''
-    tran = {'item_num':trans_tuple[0], 'amount':trans_tuple[1], 'category':trans_tuple[2], 'date':trans_tuple[3],'desc':trans_tuple[4]}
+    ''' trans is a transaction tuple ( item_num, amount, category, date, description)'''
+    tran = {'item_num':trans_tuple[0], 'amount':trans_tuple[1], 'category':trans_tuple[2],
+    'date':trans_tuple[3],'description':trans_tuple[4]}
     return tran
+
 #to_trans_dict_list -- Charlotte
 def to_trans_dict_list(trans_tuples):
     ''' convert a list of category tuples into a list of dictionaries'''
@@ -17,51 +19,49 @@ def to_trans_dict_list(trans_tuples):
 class Transaction:
     ''' connects with the sqlite database and create add, select, and delete methods --Bohan'''
 
-    # connect to the database and create the five fields: item_num, amount, category, date, description
     def __init__(self, dbfile):
+        '''connect to the database and create the five fields:
+        item_num, amount, category, date, description'''
         con = sqlite3.connect(dbfile)
         cur = con.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS transactions
-                    (item_num text, amount int, category text, date text, description text)''')
+                    (item_num text, amount int, category text, date int, description text)''')
         con.commit()
         con.close()
         self.dbfile = dbfile
 
-    # return all the transactions in the database 
     def select_all(self):
+        '''return all the transactions in the database'''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("""SELECT * from transactions""")
         rows = cur.fetchall()
         con.close()
-        #return [dict(row) for row in rows]
         return to_trans_dict_list(rows)
 
-
-    # return one transaction in the database
-    def select_one(self, id):
+    def select_one(self, item_id):
+        '''return one transaction in the database'''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("""SELECT * FROM transactions WHERE item_num = ?""", (id,))
+        cur.execute("""SELECT * FROM transactions WHERE item_num = ?""", (item_id,))
         row = cur.fetchall()
         con.close()
-        #return dict(row)
-        return to_trans_dict(row)
+        return to_trans_dict(row[0])
 
-
-    # add a transaction to the database
     def add(self, item):
+        '''add a transaction to the database'''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
         cur.execute("""INSERT INTO transactions VALUES(?,?,?,?,?)""",
-                    (item['item_num'], item['amount'], item['category'], item['date'], item['description']))
+                    (item['item_num'], item['amount'], item['category'],
+                    item['date'], item['description']))
         con.commit()
         con.close()
 
-    # delete a transaction from the database
-    def delete(self, id):
+    def delete(self, item_id):
+        '''delete a transaction from the database'''
         con = sqlite3.connect(self.dbfile)
         cur = con.cursor()
-        cur.execute("""DELETE FROM transactions WHERE item_num = ?""", (id,))
+        cur.execute("""DELETE FROM transactions WHERE item_num = ?""", (item_id,))
         con.commit()
         con.close()
